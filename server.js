352cf2527cd87c2e965b28b52ecd228fedba6a09
THIS IS MY NAME <github@0x3d.uk>
2024-07-28 21:08:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const path = require('path');
 const helmet = require('helmet');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { sequelize, models } = require('./models');
-const greenlock = require('greenlock-express');
+const { sequelize } = require('./models');
 
 dotenv.config();
 
@@ -22,7 +21,7 @@ const cspDirectives = {
         scriptSrc: ["'self'", "https://cdnjs.cloudflare.com"],
         styleSrc: ["'self'", "https://cdnjs.cloudflare.com", "'unsafe-inline'", "https://fonts.googleapis.com"],
         imgSrc: ["'self'", "data:"],
-        connectSrc: ["'self'", "https://localhost:5000"],
+        connectSrc: ["'self'", "https://ornabuilds.0x3d.uk"], // Update with your domain
         fontSrc: ["'self'", "https://fonts.gstatic.com"],
         objectSrc: ["'none'"],
         upgradeInsecureRequests: []
@@ -38,7 +37,6 @@ sequelize.authenticate()
     .then(() => console.log('PostgreSQL connected'))
     .catch(err => console.error('Unable to connect to the database:', err));
 
-// Ensure this path points to the correct 'build' directory of your frontend
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
 app.use('/api/auth', authRoutes);
@@ -51,29 +49,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
-const greenlockConfig = {
-    version: 'draft-12',
-    configDir: '/etc/letsencrypt',
-    server: 'https://acme-v02.api.letsencrypt.org/directory',
-    approveDomains: ['ornabuilds.0x3d.uk'],
-    email: 'admin@0x3d.uk',
-    agreeTos: true,
-    communityMember: false,
-    telemetry: false,
-    challenges: {
-        'http-01': require('acme-http-01-standalone')
-    }
-};
-
-// Create HTTP server for ACME HTTP-01 challenge and redirect to HTTPS
-require('http').createServer(greenlock.middleware(app)).listen(80, () => {
-    console.log(`ACME HTTP-01 challenge server running on port 80`);
-});
-
-// Create HTTPS server with Let's Encrypt
-greenlock.init(greenlockConfig).serve(app);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on https://localhost:${PORT}`);
+    console.log(`Server running on https://ornabuilds.0x3d.uk:${PORT}`);
 });
